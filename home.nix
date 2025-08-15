244beda4b19c32dda6cfc8f2385a6de1d079f89b
mikastiv <mikastiv@outlook.com>
2025-08-15 12:39:11 -0300

    refactor

--- home.nix
+++ home.nix
@@ -72,34 +72,11 @@
       vimAlias = true;
       vimdiffAlias = true;
     };
-
-    zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      defaultKeymap = "emacs";
-      shellAliases = {
-        ls = "eza";
-        cat = "bat";
-        gl = "git log --all --graph --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'";
-        nix-rebuild = "sudo nixos-rebuild switch --flake /home/${username}/.flake#nixos";
-        nix-delete-old-boot = "sudo nix-env --delete-generations +5 --profile /nix/var/nix/profiles/system";
-      };
-      initContent = ''
-        fpath=(/home/${username}/.zig-completions $fpath)
-      '';
-      history = {
-        append = true;
-        share = true;
-        findNoDups = true;
-        ignoreAllDups = true;
-        ignoreDups = true;
-        ignoreSpace = true;
-      };
-    };
   };
 
   imports = [
     ./modules/home/git.nix
+    ./modules/home/zsh.nix
   ];
 
   # This value determines the Home Manager release that your
