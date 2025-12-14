43847cbb89b58fdd8fb40c439825499b770e3794
mikastiv <mikastiv@outlook.com>
2025-12-14 14:15:37 -0400

    update

--- home.nix
+++ home.nix
@@ -105,7 +105,6 @@
         cat = "bat";
         gl = "git log --all --graph --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'";
         nix-rebuild = "sudo nixos-rebuild switch --flake /home/${username}/.flake#nixos";
-        nix-delete-old-boot = "sudo nix-env --delete-generations +5 --profile /nix/var/nix/profiles/system";
       };
       completionInit = ''
         fpath=(/home/${username}/.zig-completions $fpath)
